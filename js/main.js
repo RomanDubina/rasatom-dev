aaa3df7f5dd872069b58139836951f93ae352a3f
RomanDubina <70788091+RomanDubina@users.noreply.github.com>
2021-08-27 07:26:03 +0300

    create page

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(function(){
+
+    let header = document.querySelector('.header');
+    let scrollPrev = 0;
+    window.onscroll = function(){
+    let scrollPos = window.pageYOffset;
+     if(scrollPos>scrollPrev){
+       
+        header.style.top = '-200px'
+        header.style.backgroundColor = '#0F1435'
+     } else {
+        header.style.backgroundColor = '#0F1435'
+        header.style.top = '0px'
+     }
+    
+     scrollPrev = scrollPos;
+    
+       } 
+
+
+});
